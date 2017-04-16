624dbcfb49bd2670aee75c3f8fdb13579bf9d233
Mihai Valentin <MihaiValentin@users.noreply.github.com>
2017-04-16 15:58:35 +0300

    Added backward compatibility comment

--- lunr.jp.js
+++ lunr.jp.js
@@ -1,2 +1,5 @@
-// for backward-compatibility
+// jp is the country code, while ja is the language code
+// a new lunr.ja.js has been created, but in order to
+// keep the backward compatibility, we'll leave the lunr.jp.js
+// here for a while, and just make it use the new lunr.ja.js
 module.exports = require('./lunr.ja');
