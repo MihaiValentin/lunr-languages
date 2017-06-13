84aa13a04297c8dba44fe05d8e475ba770a0d539
keerati <keerati.th@gmail.com>
2017-06-13 23:41:31 +0700

    fix & revise test cases

--- test/testdata/th.js
+++ test/testdata/th.js
@@ -10,34 +10,27 @@ module.exports = {
     documents: [
         {
             "title": "สวัสดี",
-            "body" : "สวัสดีชาวโลก",
+            "body" : "ชาวบ้านในหมู่บ้านกล่าวพร้อมกันว่าสวัสดี",
             "id": 1
         }, {
-            "title": "ฉัน",
-            "body" : "ชาวบ้านกล่าวพร้อมกันว่าสวัสดี",
+            "title": "สุภาษิต",
+            "body" : "ในน้ำมีปลา ในนามีข้าว แผ่นดินของเราที่แสนอุดมสมบูรณ์",
             "id": 2
         }
     ],
     tests: [
         {
             what: "find the word %w",
-            search: "สวัสดี",
+            search: "ใน",
             found: 2
         }, {
             what: "find the word %w",
-            search: "บ้าน",
+            search: "แผ่นดิน",
             found: 1
         }, {
             what: "never find a word that does not exist, like %w",
             search: "เบื่อ",
             found: 0
         } 
-        /*
-        {
-            what: "find a correctly stemmed word %w",
-            search: "opparbeider",
-            found: 1
-        }
-        */
     ]
 }
